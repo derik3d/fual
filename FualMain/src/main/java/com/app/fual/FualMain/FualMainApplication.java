9ad0c458b185c95ebdd2519bbee53e481c47a4c7
ximena <ximena@192.168.0.15>
2019-10-06 19:59:36 -0500

    FIRST COMMIT

---
+++ FualMain/src/main/java/com/app/fual/FualMain/FualMainApplication.java
@@ -0,0 +1,13 @@
+package com.app.fual.FualMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FualMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FualMainApplication.class, args);
+	}
+
+}
---
+++ FualMain/src/test/java/com/app/fual/FualMain/FualMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fual.FualMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FualMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
