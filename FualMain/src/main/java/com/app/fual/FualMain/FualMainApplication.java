66334c550c497acff4b54f1264a7568f3a8ebb08
ximena <ximena@192.168.0.15>
2019-10-06 20:09:10 -0500

    corrected file

--- FualMain/src/main/java/com/app/fual/FualMain/FualMainApplication.java
+++ FualMain/src/main/java/com/app/fual/FualMain/FualMainApplication.java
@@ -1,17 +1,13 @@
 package com.app.fual.FualMain;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Controller
-public class FualMainApplication{
+@SpringBootApplication
+public class FualMainApplication {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FualMainApplication.class, args);
+	}
 
 }
