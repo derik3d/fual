4de334ff268ddf34013682dae75194d76fd9de9b
ximena <ximena@192.168.0.15>
2019-10-23 12:06:27 -0500

    generl endpoints added

--- FualMain/src/main/java/com/app/fual/FualMain/Controllers/GeneralAPI.java
+++ FualMain/src/main/java/com/app/fual/FualMain/Controllers/GeneralAPI.java
@@ -1,12 +1,17 @@
 package com.app.fual.FualMain.Controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,16 +26,23 @@ public abstract class GeneralAPI<T> {
 	@Autowired
 	IManagerServiceGenerics<T> iManagerServiceGenerics;
 	
+	
+
     @GetMapping
+    public ResponseEntity<List<T>> getAllEntities(){
+
+    	List<T> entities = iManagerServiceGenerics.getAll();
+    	
+    	if (entities == null)return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    	else return new ResponseEntity<>(entities,HttpStatus.OK);
+    }
+	
+	
+    @GetMapping("{id}")
     public ResponseEntity<T> getEntity(
-      @RequestParam(name="id") Long id) {
-      
-    	//Class<T> myClass;
-    	//T t = myClass.newInstance();
+      @PathVariable(name="id") Long id) {
     	
     	T entity = null;
-    	
-    	System.out.println("first step reached");
     	
     	entity = iManagerServiceGenerics.findEntity( entity , id);
     	
@@ -47,5 +59,24 @@ public abstract class GeneralAPI<T> {
     	if (entity == null)return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     	else return new ResponseEntity<>(entity,HttpStatus.OK);
 	}
+    
+    @PutMapping("{id}")
+    public ResponseEntity<T> update(
+    	      @PathVariable(name="id") Long id, @RequestBody T entity) {
+    	entity = iManagerServiceGenerics.updateEntity(id, entity);
+    	
+    	if (entity == null)return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    	else return new ResponseEntity<>(entity,HttpStatus.OK);
+    }
+    
+    @DeleteMapping
+    public ResponseEntity delete(
+  	      @PathVariable(name="id") Long id) {
+  	boolean result = iManagerServiceGenerics.deleteEntity(id);
+  	
+  	if (result)return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+  	else return ResponseEntity.ok(HttpStatus.OK);
+  }
+    
 
 }
--- FualMain/src/main/java/com/app/fual/FualMain/Interfaces/IManagerServiceGenerics.java
+++ FualMain/src/main/java/com/app/fual/FualMain/Interfaces/IManagerServiceGenerics.java
@@ -1,8 +1,13 @@
 package com.app.fual.FualMain.Interfaces;
 
+import java.util.List;
+
 public interface IManagerServiceGenerics<T> {
 
 	T createEntity(T entity);
 	T findEntity(T entitySample, Long id);
+	T updateEntity(Long id, T entity);
+	boolean deleteEntity(Long id);
+	List<T> getAll();
 
 }
