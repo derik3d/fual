bdc9de719f8845c73266fd8a6cb588e8960b121d
ximena <ximena@192.168.0.15>
2019-11-21 20:08:00 -0500

    challenge updated

--- FualMain/src/main/java/com/app/fual/FualMain/DTO/ChallengeDTO.java
+++ FualMain/src/main/java/com/app/fual/FualMain/DTO/ChallengeDTO.java
@@ -29,66 +29,7 @@ public class ChallengeDTO {
 	
 	private String sets;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + ((duration == null) ? 0 : duration.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((level == null) ? 0 : level.hashCode());
-		result = prime * result + ((movement == null) ? 0 : movement.hashCode());
-		result = prime * result + ((sets == null) ? 0 : sets.hashCode());
-		result = prime * result + ((workout == null) ? 0 : workout.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ChallengeDTO other = (ChallengeDTO) obj;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (duration == null) {
-			if (other.duration != null)
-				return false;
-		} else if (!duration.equals(other.duration))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (level == null) {
-			if (other.level != null)
-				return false;
-		} else if (!level.equals(other.level))
-			return false;
-		if (movement == null) {
-			if (other.movement != null)
-				return false;
-		} else if (!movement.equals(other.movement))
-			return false;
-		if (sets == null) {
-			if (other.sets != null)
-				return false;
-		} else if (!sets.equals(other.sets))
-			return false;
-		if (workout == null) {
-			if (other.workout != null)
-				return false;
-		} else if (!workout.equals(other.workout))
-			return false;
-		return true;
-	}
+	private String rest;
 
 	public Long getId() {
 		return id;
@@ -146,10 +87,85 @@ public class ChallengeDTO {
 		this.sets = sets;
 	}
 
+	public String getRest() {
+		return rest;
+	}
+
+	public void setRest(String rest) {
+		this.rest = rest;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((duration == null) ? 0 : duration.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((level == null) ? 0 : level.hashCode());
+		result = prime * result + ((movement == null) ? 0 : movement.hashCode());
+		result = prime * result + ((rest == null) ? 0 : rest.hashCode());
+		result = prime * result + ((sets == null) ? 0 : sets.hashCode());
+		result = prime * result + ((workout == null) ? 0 : workout.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChallengeDTO other = (ChallengeDTO) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (duration == null) {
+			if (other.duration != null)
+				return false;
+		} else if (!duration.equals(other.duration))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (level == null) {
+			if (other.level != null)
+				return false;
+		} else if (!level.equals(other.level))
+			return false;
+		if (movement == null) {
+			if (other.movement != null)
+				return false;
+		} else if (!movement.equals(other.movement))
+			return false;
+		if (rest == null) {
+			if (other.rest != null)
+				return false;
+		} else if (!rest.equals(other.rest))
+			return false;
+		if (sets == null) {
+			if (other.sets != null)
+				return false;
+		} else if (!sets.equals(other.sets))
+			return false;
+		if (workout == null) {
+			if (other.workout != null)
+				return false;
+		} else if (!workout.equals(other.workout))
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
 		return "ChallengeDTO [id=" + id + ", level=" + level + ", description=" + description + ", duration=" + duration
-				+ ", movement=" + movement + ", workout=" + workout + ", sets=" + sets + "]";
+				+ ", movement=" + movement + ", workout=" + workout + ", sets=" + sets + ", rest=" + rest + "]";
 	}
 	
 	
